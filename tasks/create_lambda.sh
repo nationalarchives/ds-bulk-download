2be6ea06735979c1c56a3ac93bf41eb5fddb4833
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-06-23 16:52:49 +0100

    Fix MinIO port mapping and update create_lambda.sh to use pip3 with specific options

--- tasks/create_lambda.sh
+++ tasks/create_lambda.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+rm -fR ./lambda/*
 mkdir -p ./lambda/package
 cp ./tasks/process.py ./lambda
 cd ./lambda
 mv process.py lambda_function.py
-pip install --target ./package boto3 pydantic
+pip3 install --target ./package --platform manylinux2014_x86_64 --python-version 3.14 --only-binary=:all: --root-user-action=ignore pydantic
 cd package
 zip -r ../bulk_downloads_processor.zip .
 cd ..
